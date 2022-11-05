4613b4171bf05627aab4461dde08184b516308be
rmeiv1754 <110749847+rmeiv1754@users.noreply.github.com>
2022-11-04 23:08:10 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,78 @@
+/* --------------------------- NO TOCAR DESDE ACÁ --------------------------- */
+let datosPersona = {
+  nombre: "",
+  edad: 0,
+  ciudad: "",
+  interesPorJs: "",
+};
+
+const listado = [{
+    imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
+    lenguajes: "HTML y CSS",
+    bimestre: "1er bimestre",
+  },
+  {
+    imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
+    lenguajes: "Javascript",
+    bimestre: "2do bimestre",
+  },
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
+    lenguajes: "React JS",
+    bimestre: "3er bimestre",
+  },
+];
+
+const profileBtn = document.querySelector("#completar-perfil");
+const materiasBtn = document.querySelector("#obtener-materias");
+const verMasBtn = document.querySelector("#ver-mas");
+const cambiarTema = document.querySelector('#cambiar-tema');
+
+profileBtn.addEventListener("click", renderizarDatosUsuario);
+materiasBtn.addEventListener("click", recorrerListadoYRenderizarTarjetas);
+cambiarTema.addEventListener("click", alternarColorTema);
+/* --------------------------- NO TOCAR HASTA ACÁ --------------------------- */
+
+function obtenerDatosDelUsuario() {
+  /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
+  let nombreP = prompt("Ingresa tu nombre");
+  console.log(nombreP);
+  let añoP = prompt("Ingresa el año en que naciste");
+  añoP = 2022 - añoP;
+  console.log(añoP);
+  let ciudadP = prompt("Ingresa la ciudad en donde vives");
+  console.log(ciudadP);
+  let interesP = confirm("¿Te interesa JavaScipt?");
+  console.log(interesP);
+
+}
+
+
+function renderizarDatosUsuario() {
+  /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
+  obtenerDatosDelUsuario();
+  /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
+  console.log(obtenerDatosDelUsuario);
+  const cardHeader = document.getElementById('nombre').value = `${obtenerDatosDelUsuario.nombreP}`;
+  console.log(cardHeader);
+
+}
+
+
+function recorrerListadoYRenderizarTarjetas() {
+  /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
+  
+
+
+}
+
+function alternarColorTema() {
+  /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
+ 
+  
+
+
+}
+
+/* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
+
